0c51ff88abbb30e788a59f0227129540d8cdfccd
ber01 <ber01@naver.com>
2019-09-23 16:32:58 +0900

    POST /api/events return 201

--- src/test/java/com/kyunghwan/demorestapi/events/EventControllerTests.java
+++ src/test/java/com/kyunghwan/demorestapi/events/EventControllerTests.java
@@ -1,15 +1,22 @@
 package com.kyunghwan.demorestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -19,12 +26,37 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    EventRepository eventRepository;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST-API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019, 9, 19, 13, 0))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 9, 20, 13, 0))
+                .beginEventDateTime(LocalDateTime.of(2019, 9, 21, 13, 0))
+                .endEventDateTime(LocalDateTime.of(2019, 9, 22, 13, 0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("경성대학교 건학기념관")
+                .build();
+
+        event.setId(10);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
         mockMvc.perform(post("/api/events/")
                     .contentType(MediaType.APPLICATION_JSON_UTF8)
-                    .accept(MediaTypes.HAL_JSON))
-                .andExpect(status().isCreated());
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists());
     }
 
 }
