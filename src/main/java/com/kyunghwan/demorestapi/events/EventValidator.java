9135d4131560aec15f95fee7dee40fc467dc8f7c
ber01 <ber01@naver.com>
2019-09-24 14:05:17 +0900

    Add test description

--- src/main/java/com/kyunghwan/demorestapi/events/EventValidator.java
+++ src/main/java/com/kyunghwan/demorestapi/events/EventValidator.java
@@ -3,14 +3,13 @@ package com.kyunghwan.demorestapi.events;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Component
 public class EventValidator {
 
     public void validate(EventDto eventDto, Errors errors){
-        if (eventDto.getBasePrice() > eventDto.getMaxPrice() || eventDto.getMaxPrice() > 0){
+        if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() != 0){
             errors.rejectValue("basePrice", "wrongValue", "BasePrice is Wrong");
             errors.rejectValue("maxPrice", "wrongValue", "MaxPrice is Wrong");
         }
@@ -34,7 +33,5 @@ public class EventValidator {
         if (closeEnrollmentDateTime.isBefore(eventDto.getBeginEnrollmentDateTime())) {
             errors.rejectValue("closeEnrollmentDateTime", "wrongValue", "CloseEnrollmentDateTime is Wrong");
         }
-
-
     }
 }
