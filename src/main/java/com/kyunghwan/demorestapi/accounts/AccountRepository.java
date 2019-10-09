9fded9a3e506a99abc02a4458670970a43d9405c
ber01 <ber01@naver.com>
2019-10-10 08:48:14 +0900

    Add Account, SecurityConfig

--- src/main/java/com/kyunghwan/demorestapi/accounts/AccountRepository.java
+++ src/main/java/com/kyunghwan/demorestapi/accounts/AccountRepository.java
@@ -1,7 +1,6 @@
 package com.kyunghwan.demorestapi.accounts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
--- src/test/java/com/kyunghwan/demorestapi/accounts/AccountServiceTest.java
+++ src/test/java/com/kyunghwan/demorestapi/accounts/AccountServiceTest.java
@@ -1,28 +1,38 @@
 package com.kyunghwan.demorestapi.accounts;
 
+import jdk.jfr.Description;
+import org.hamcrest.Matchers;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @ActiveProfiles("test")
 public class AccountServiceTest {
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Autowired
     AccountService accountService;
 
     @Autowired
-    AccountRepository accountRepository;
+    PasswordEncoder passwordEncoder;
 
     @Test
     public void findByUsername() {
@@ -36,14 +46,46 @@ public class AccountServiceTest {
                 .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
                 .build();
 
-        this.accountRepository.save(account);
+        this.accountService.saveAccount(account);
 
         // When
         UserDetailsService userDetailsService = accountService;
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
         // Then
-        assertThat(userDetails.getPassword()).isEqualTo(password);
+        assertThat(this.passwordEncoder.matches(password, userDetails.getPassword())).isTrue();
+    }
+
+    @Test(expected = UsernameNotFoundException.class)
+    @Description("유저가 없는 경우 예외 발생 방법1 - expected")
+    public void findByUsernameFail1() {
+        String username = "failName@gmail.com";
+        accountService.loadUserByUsername(username);
+    }
+
+    @Test
+    @Description("유저가 없는 경우 예외 발생 방법2 - try, catch")
+    public void findByUsernameFail2() {
+        String username = "failName@gmail.com";
+
+        try {
+            accountService.loadUserByUsername(username);
+            fail("테스트 실패");
+        } catch (UsernameNotFoundException e) {
+            assertThat(e.getMessage()).containsSequence(username);
+        }
+    }
+
+    @Test
+    @Description("유저가 없는 경우 예외 발생 방법3 - @Rule, ExpectedException")
+    public void findByUsernameFail3() {
+        // Expected
+        String username = "failName@gmail.com";
+        expectedException.expect(UsernameNotFoundException.class);
+        expectedException.expectMessage(Matchers.containsString(username));
+
+        // When
+        accountService.loadUserByUsername(username);
     }
 
 }
